e4f427eabc541f64cf1b8c6d69e22b80033236fc
Krzysztof Grabczyński <grabczynski.krzysztof@gmail.com>
2022-11-25 15:57:54 +0100

    Adding class TestsShip in ship_tests.py. Adding test cases for checking if the setters Ship's class are working correctlly

---
+++ tests/ship_tests.py
@@ -0,0 +1,28 @@
+import pytest
+from sys import path
+path.append(r'C:\Users\kgrab\OneDrive\Pulpit\Python\Klasy\class_ship_game')
+from ship import Ship
+
+class TestsShip:
+    def test_ship_speed_setter_with_correct_value(self):
+        ship = Ship(1, 'Statek_1', 5, 10, 10) 
+        ship.speed = 10
+        assert ship.speed == 10
+    def test_ship_speed_setter_with_incorrect_value(self):
+        ship = Ship(1, 'Statek_1', 5, 10, 10)
+        with pytest.raises(ValueError):  
+            ship.speed = -1
+
+    def test_ship_max_crew_setter_with_correct_value(self):
+        ship = Ship(1, 'Statek_1', 5, 10, 10) 
+        ship.max_crew = 10
+        assert ship.max_crew == 10
+    def test_ship_max_crew_setter_with_incorrect_value_v1(self):
+        ship = Ship(1, 'Statek_1', 5, 10, 10)
+        with pytest.raises(ValueError):  
+            ship.max_crew = -1  
+    def test_ship_max_crew_setter_with_incorrect_value_v2(self):
+        ship = Ship(1, 'Statek_1', 5, 10, 10)
+        with pytest.raises(ValueError):  
+            ship.max_crew = 8.5
+
