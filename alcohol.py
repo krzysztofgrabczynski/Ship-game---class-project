41c1266746774bb5421a4ef5a291e54fc5391ae7
Krzysztof Grabczyński <grabczynski.krzysztof@gmail.com>
2022-11-26 21:50:26 +0100

    Adding body of the class Alcohol

---
+++ alcohol.py
@@ -0,0 +1,24 @@
+from cargo import Cargo
+
+
+class Alcohol(Cargo):
+    def __init__(self, name, amount, base_price, percentage = 96):
+        super().__init__(name, amount, base_price)
+        self.__percentage = percentage
+        self.__base_percentage = 96
+
+    # properties
+
+    @property
+    def percentage(self):
+        return self.__percentage
+
+    # methods
+    
+    def __repr__(self):
+        return super().__repr__() + "\n" + "Percentage: {}".format(self.__percentage)
+        
+    def getPrice(self):
+        return self.base_price * (self.__percentage / self.__base_percentage)
+
+
