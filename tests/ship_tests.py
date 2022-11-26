73c506b1d3f36bdd1f014cf2efdeec5c10bd5447
Krzysztof Grabczyński <grabczynski.krzysztof@gmail.com>
2022-11-26 11:09:47 +0100

    Adding new test in ship_tests.py. Testing adding crew by using '+=' operator on Ship class object

--- tests/ship_tests.py
+++ tests/ship_tests.py
@@ -18,7 +18,7 @@ class TestsShip:
         ship.max_crew = 10
         assert ship.max_crew == 10
     def test_ship_max_crew_setter_with_incorrect_value_v1(self):
-        ship = Ship(1, 'Statek_1', 5, 10, 10)
+        ship = Ship(1, 'Statek_1', 5, 10, 10) 
         with pytest.raises(ValueError):  
             ship.max_crew = -1  
     def test_ship_max_crew_setter_with_incorrect_value_v2(self):
@@ -26,3 +26,13 @@ class TestsShip:
         with pytest.raises(ValueError):  
             ship.max_crew = 8.5
 
+    def test_ship_adding_crew_with_correct_value(self):
+        ship = Ship(1, 'Statek_1', 5, 10, 10) 
+        ship.max_crew = 20
+        ship += 15
+        assert ship.crew == 15
+    def test_ship_adding_crew_with_incorrect_value(self):
+        ship = Ship(1, 'Statek_1', 5, 10, 10) 
+        ship.max_crew = 5
+        with pytest.raises(ValueError):  
+            ship += 30
