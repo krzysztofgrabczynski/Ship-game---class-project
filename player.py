6befae2e26abf0d52ad0bad3c516524ebf760735
Krzysztof Grabczyński <grabczynski.krzysztof@gmail.com>
2022-11-29 18:53:55 +0100

    Updating player.py. Adding body of the class: Player

--- player.py
+++ player.py
@@ -1,2 +1,22 @@
+from ship import Ship
+
 class Player:
-    pass
+    def __init__(self, name: str, ship: Ship, money: int):
+        self.__name = name
+        self.ship = ship
+        self.__money = money
+
+    # properties
+
+    @property
+    def name(self):
+        return self.__name
+
+    @property
+    def money(self):
+        return self.__money
+
+    @money.setter
+    def money(self, money):
+        self.__money = money
+    
