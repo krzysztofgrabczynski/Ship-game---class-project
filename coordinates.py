adc17cc5111492ee4f3ea27b4fc5626818130121
Krzysztof Grabczyński <grabczynski.krzysztof@gmail.com>
2022-12-02 14:33:14 +0100

    Updating coordinates.py file

--- coordinates.py
+++ coordinates.py
@@ -1,24 +1,26 @@
 from random import randint
-
+from math import sqrt
 
 class Coordinates:
 
     __list_of_used_coordinates = []
+    __map_width = 400
+    __map_height = 400
 
     def random_cords(self):
         while True:
-            x = randint(0, 400)
-            y = randint(0, 400)
+            x = randint(0, self.__map_width)
+            y = randint(0, self.__map_height)
             cord = (x, y)
             if not cord in self.__list_of_used_coordinates:
                 self.__list_of_used_coordinates.append(cord)
-                print(cord, x, y)
-                break
-        return cord
-
+                return cord
+        
+        
     def __init__(self):
-        self.__x = self.random_cords()[0]
-        self.__y = self.random_cords()[1]
+        self.__cords = self.random_cords()
+        self.__x = self.__cords[0]
+        self.__y = self.__cords[1]
 
 
     # properties
@@ -26,10 +28,27 @@ class Coordinates:
     @property
     def x(self):
         return self.__x
-    
+    @x.setter
+    def x(self, new_x):
+        self.__x = new_x
+
     @property
     def y(self):
         return self.__y
-        
-cord = Coordinates()
-print(vars(cord))
+    @y.setter
+    def y(self, new_y):
+        self.__y = new_y
+
+    # methods
+
+    def __repr__(self) -> str:
+        return f'Coordinates: {self.get_coordinates()}'
+
+    def get_coordinates(self):
+        return (self.__x, self.__y)
+
+    def distance(self, other):
+        return sqrt((self.__x - other.x) ** 2 + (self.__y - other.y) ** 2)
+
+
+
