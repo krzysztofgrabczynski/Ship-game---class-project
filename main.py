2f3936e22eff8dd8b4b01f12dd5f18837974bf5d
Krzysztof Grabczyński <grabczynski.krzysztof@gmail.com>
2022-12-02 19:21:02 +0100

    Adding body of the game in main.py file

--- main.py
+++ main.py
@@ -5,14 +5,56 @@ from item import Item
 from cargo import Cargo
 from store import Store
 from player import Player
+from map import Map
 
 
-ship = Ship(1, 'Statek_1', 10, 30, 1000)
-ship += 15
-alcohol = Alcohol('Spirytus', 100, 27, 96)
-fruit = Fruit('banan', 10, 5)
-item = Item('miecz', 1, 20, Item.Rarity.rare)
-player = Player('Krzysztof', ship, 1000)
+#####################################
+############# GAME BODY #############
+#####################################
+
+# bilding map with islands
+map = Map(3, 8)         
+print('\n\n', 100*'#', '\n', 'List of islands:\n', map.list_of_islands)
+print(100*'#', '\n', 'Coordinates of first island:\n', map.list_of_islands[0].coordinates)
+print(100*'#', '\n', 'Coordinates of second island:\n', map.list_of_islands[1].coordinates)
+print(100*'#', '\n', 'Inventory in the store on second island:\n',map.list_of_islands[1].store)
+print('\n')
+# creating player with specific ship
+player = Player('Jon', Ship(1, 'Victory', 200, 100, 1000), 5000)
+print(100*'#', '\n', 'Player name and money amount: ', player.name, player.money)
+print(100*'#', '\n', f"Player's ship name: {player.ship.name}, speed: {player.ship.speed}, max crew: {player.ship.max_crew}, capacity: {player.ship.capacity}\n")
+# adding crew into player's ship
+player.ship += 80
+print(100*'#', '\n', f"Player's ship crew: {player.ship.crew}\n"),
+print(100*'#', '\n', "Player's ship coordinates: ", player.ship.coordinates)
+# adding cargo into player's ship
+fruit = Fruit('apple', 40, 5)
+player.ship.load(fruit)
+print(100*'#', '\n', "Player's ship cargo: \n", player.ship.cargo)
+# checking rotten time of fruit
+print(100*'#', '\n', f"Rotten time of {fruit.name} before substracting: ", fruit.rotten_time)
+fruit - 1
+print(f"Rotten time of {fruit.name} after substracting: ", fruit.rotten_time)
+# checking distance between ship and first island
+print(100*'#', '\n', f"Ship coordinates: {player.ship.coordinates} and first island coordiantes: {map.list_of_islands[0].coordinates}")
+print("Distance: ", player.ship.coordinates.distance(map.list_of_islands[0].coordinates))
+
+
+
+#####################################
+########### GAME BODY END ###########
+#####################################
+
+
+
+### TESTS ###
+
+# ship = Ship(1, 'Statek_1', 10, 30, 1000)
+# ship += 15
+# alcohol = Alcohol('Spirytus', 100, 27, 96)
+# fruit = Fruit('banan', 10, 5)
+# item = Item('miecz', 1, 20, Item.Rarity.rare)
+# player = Player('Krzysztof', ship, 1000)
 
 # print(ship)
 # print('-' * 30)
@@ -44,6 +86,6 @@ player = Player('Krzysztof', ship, 1000)
 # print(30*'-')
 # print(ship.cargo)
 
-print(ship.coordinates)
-print(player.ship.coordinates.x)
-print(ship.coordinates.x)
+# print(ship.coordinates)
+# print(player.ship.coordinates.x)
+# print(ship.coordinates.x)
