4864d4ba077c8c89d3f030e448552d24e0ffa088
Krzysztof Grabczyński <grabczynski.krzysztof@gmail.com>
2022-12-02 19:20:01 +0100

    Adding body of the Map class in map.py file: adding random_island func and property with the same name

--- map.py
+++ map.py
@@ -1,2 +1,25 @@
+from random import randint
+from island import Island
+from store import Store
+
+
 class Map:
-    pass
+    
+    __list_of_islands = []
+
+    def __random_island(self, min_island_number, max_island_number) -> None:
+        number_of_islands = randint(min_island_number, max_island_number)
+
+        for i in range(number_of_islands):
+            self.__list_of_islands.append(Island(f'Island_{i+1}', Store(f'Store_{i+1}')))
+            self.__list_of_islands[i].store.get_random_cargo()
+        
+    def __init__(self, min_island_number, max_island_number) -> None:
+        self.__random_island(min_island_number, max_island_number)
+
+
+    # properties
+
+    @property
+    def list_of_islands(self):
+        return self.__list_of_islands
